1aea18e39103a7b2e9a8ab507fec305719087f1f
857047515 <g7244858@gmail.com>
2026-06-24 00:55:21 +0800

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '')));
-app.get('/', (req,res)=>res.sendFile(path.join(__dirname,'/index.html')));
-app.listen(PORT, ()=>console.log(`自主學習系統：http://localhost:${PORT}`));
